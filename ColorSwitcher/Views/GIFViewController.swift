caebc97aaa8510e94f08b1cffd00cae2eeeafee8
matt smith <matt.smith@kroger.com>
2020-09-28 12:25:50 -0700

    Created the storyboard and it's associated class
    Hooked up segue to new view

---
+++ ColorSwitcher/Views/GIFViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GIFViewController.swift
+//  ColorSwitcher
+//
+//  Created by ms70285 on 9/28/20.
+//
+
+import UIKit
+
+class GIFViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
