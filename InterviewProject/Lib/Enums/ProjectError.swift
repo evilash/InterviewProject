4f6bd14d3621eb606d1073df29e3346ac531603b
matt smith <rubys_place@hotmail.com>
2020-11-05 22:39:54 -0800

    Updated urlString names and also removed redundant true false return
    Added another error to the enum

--- InterviewProject/Lib/Enums/ProjectError.swift
+++ InterviewProject/Lib/Enums/ProjectError.swift
@@ -10,5 +10,6 @@ import Foundation
 enum ProjectError: String, Error {
     case gifURLIsNil = "Sorry, but we can't find your GIF. Please try again later."
     case networkError = "Please connect to the internet and try again."
-    case buttonTitleIsNil = "The button title is missing text"
+    case buttonTitleIsNil = "The button title is missing text."
+    case technicalDifficulties = "Sorry, but we are having technical difficulties."
 }
