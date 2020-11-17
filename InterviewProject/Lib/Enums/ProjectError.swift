6cd3e0e7e0c637619baefff6ea01eb2b601459a5
matt smith <matt.smith@kroger.com>
2020-11-17 13:13:25 -0800

    Updated Alert.create(:message:vc) to have a default title

--- InterviewProject/Lib/Enums/ProjectError.swift
+++ InterviewProject/Lib/Enums/ProjectError.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 enum ProjectError: String, Error {
+    case title = "ERROR"
     case gifURLIsNil = "Sorry, but we can't find your GIF. Please try again later."
     case networkError = "Please connect to the internet and try again."
     case buttonTitleIsNil = "The button title is missing text."
