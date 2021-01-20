1d8c8c48ff1876d8b30b471202eba2feec0614b2
matt smith <rubys_place@hotmail.com>
2021-01-19 22:35:02 -0800

    Adding combine framework to the app
    I've setup the app to use combine to make network calls. I also broke out the properties used for storing Tenor API info into their own struct. 
    Removed the error gif from the project since it was no longer needed.
    I think I still need to make a couple of tweaks, but it works!

---
+++ InterviewProject/Network/Tenor.swift
@@ -0,0 +1,14 @@
+//
+//  Tenor.swift
+//  InterviewProject
+//
+//  Created by Matt Smith on 1/19/21.
+//
+
+import Foundation
+
+struct Tenor {
+    static let apiKey = "0OZKMCWSOD4M"
+    static let baseURL = "https://api.tenor.com/v1/search"
+    static let filters = "contentfilter=high&media_filter=minimal&limit=1"
+}
